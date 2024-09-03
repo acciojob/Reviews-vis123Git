6151b09e30e6c727bc3157b6a595428ed522b6db
VishalKumarSain <vishaldhoria007@gmail.com>
2024-09-03 21:37:44 +0530

    second

--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,22 @@
-import React from "react";
+import React, { useState } from "react";
 import Review from "./Review";
 import data from "../data.js";
-import "../styles/App.css"
+import "../styles/App.css";
 function App() {
+  const [review, setReview] = useState(data[0]);
+  const nextHandleReview = ()=>{
+
+        console.log("review===",review)
+}
   return (
     <div className="container">
       <h1 id="review-heading">Our Reviews</h1>
       <button className="random-btn">surprise me</button>
-      {data.map((el) => {
-        return <Review key={el.id} review={el} />;
-      })}
+      <button className='prev-btn'>Previous</button>
+
+      <Review key={review.id} review={review} />;
+      <button className='next-btn' onClick={nextHandleReview(review)}>Next</button>
+
     </div>
   );
 }
--- src/components/Review.js
+++ src/components/Review.js
@@ -1,16 +1,16 @@
-import React from 'react'
-import "../styles/review.css"
-function Review({review}) {
+import React from "react";
+import "../styles/review.css";
+function Review({ review }) {
   return (
-    <div className='review'>
-        <button className='prev-btn'>Previous</button>
-        <p className='author' id={`author-${review.id}`}>{review.name}</p>
-        <p className='job'>{review.job}</p>
-        <p className='info'>{review.text}</p>
-        <img className='person-img' src={review.image}/>
-        <button className='next-btn'>Next</button>
+    <div className="review">
+      <p className="author" id={`author-${review.id}`}>
+        {review.name}
+      </p>
+      <p className="job">{review.job}</p>
+      <p className="info">{review.text}</p>
+      <img className="person-img" src={review.image} />
     </div>
-  )
+  );
 }
 
-export default Review
+export default Review;
